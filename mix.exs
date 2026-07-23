ff78559574c5fccbbf0fecf56b96aafba79e5ee1
dismad <81990132+dismad@users.noreply.github.com>
2026-07-23 10:52:23 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -32,35 +32,37 @@ defmodule ZcashExplorer.MixProject do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [
-      {:phoenix_view, "~> 2.0", override: true},
-      {:phoenix, "~> 1.6"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.11"},
-      {:postgrex, ">= 0.0.0"},
-      {:ecto_psql_extras, "~> 0.7.14"},
-      {:phoenix_html, "~> 3.2"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.8"},
-      {:telemetry_metrics, "~> 0.6.1"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:httpoison, "~> 1.8"},
-      {:poison, "~> 3.1"},
-      {:observer_cli, "~> 1.6"},
-      {:cachex, "~> 3.3"},
-      {:phoenix_live_view, "~> 0.20"},
-      {:floki, ">= 0.27.0", only: :test},
-      {:zcashex, github: "zcash/zcashex"},
-      {:timex, "~> 3.0"},
-      {:sizeable, "~> 1.0"},
-      {:eqrcode, "~> 0.1.8"},
-      {:contex, "~> 0.3.0"},
-      {:muontrap, "~> 0.6.1"}
-    ]
-  end
+  [
+    {:phoenix_view, "~> 2.0", override: true},
+    {:phoenix, "~> 1.7"},
+    {:phoenix_ecto, "~> 4.4"},
+    {:ecto_sql, "~> 3.11"},
+    {:postgrex, ">= 0.22.3"},
+    {:ecto_psql_extras, "~> 0.8"},
+    {:phoenix_html, "~> 3.2"},
+    {:phoenix_live_reload, "~> 1.2", only: :dev},
+    {:phoenix_live_dashboard, "~> 0.8"},
+    {:telemetry_metrics, "~> 0.6.1"},
+    {:telemetry_poller, "~> 1.0"},
+    {:gettext, "~> 0.11"},
+    {:jason, "~> 1.0"},
+    {:plug_cowboy, "~> 2.7"},
+    {:httpoison, "~> 1.8"},
+   # {:hackney, "~> 4.7", override: true},
+   # {:decimal, "~> 3.1", override: true},
+    {:poison, "~> 3.1"},
+    {:observer_cli, "~> 1.6"},
+    {:cachex, "~> 3.3"},
+    {:phoenix_live_view, "~> 0.20"},
+    {:floki, ">= 0.27.0", only: :test},
+    {:zcashex, github: "zcash/zcashex"},
+    {:timex, "~> 3.0"},
+    {:sizeable, "~> 1.0"},
+    {:eqrcode, "~> 0.1.8"},
+    {:contex, "~> 0.3.0"},
+    {:muontrap, "~> 0.6.1"}
+  ]
+end
 
   # Aliases are shortcuts or tasks specific to the current project.
   # For example, to install project dependencies and perform other setup tasks, run:
