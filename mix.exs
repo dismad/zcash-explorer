a58cae44346fd3aa3dc2380d6aad2e97368377b2
dismad <81990132+dismad@users.noreply.github.com>
2026-07-23 12:23:49 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -14,9 +14,6 @@ defmodule ZcashExplorer.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {ZcashExplorer.Application, []},
@@ -24,52 +21,45 @@ defmodule ZcashExplorer.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
-  [
-    {:phoenix_view, "~> 2.0", override: true},
-    {:phoenix, "~> 1.7"},
-    {:phoenix_ecto, "~> 4.4"},
-    {:ecto_sql, "~> 3.11"},
-    {:postgrex, ">= 0.22.3"},
-    {:ecto_psql_extras, "~> 0.8"},
-    {:phoenix_html, "~> 3.2"},
-    {:phoenix_live_reload, "~> 1.2", only: :dev},
-    {:phoenix_live_dashboard, "~> 0.8"},
-    {:telemetry_metrics, "~> 0.6.1"},
-    {:telemetry_poller, "~> 1.0"},
-    {:gettext, "~> 0.11"},
-    {:jason, "~> 1.0"},
-    {:plug_cowboy, "~> 2.7"},
-    {:httpoison, "~> 1.8"},
-   # {:hackney, "~> 4.7", override: true},
-   # {:decimal, "~> 3.1", override: true},
-    {:poison, "~> 3.1"},
-    {:observer_cli, "~> 1.6"},
-    {:cachex, "~> 3.3"},
-    {:phoenix_live_view, "~> 0.20"},
-    {:floki, ">= 0.27.0", only: :test},
-    {:zcashex, github: "zcash/zcashex"},
-    {:timex, "~> 3.0"},
-    {:sizeable, "~> 1.0"},
-    {:eqrcode, "~> 0.1.8"},
-    {:contex, "~> 0.3.0"},
-    {:muontrap, "~> 0.6.1"}
-  ]
-end
+    [
+      {:phoenix_view, "~> 2.0", override: true},
+      {:phoenix, "~> 1.7"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.11"},
+      {:ecto, "~> 3.11"},
+      {:postgrex, ">= 0.22.3"},
+      {:ecto_psql_extras, "~> 0.8"},
+      {:phoenix_html, "~> 3.2"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.8"},
+      {:telemetry_metrics, "~> 0.6.1"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:plug, "~> 1.16"},
+      {:plug_cowboy, "~> 2.7"},
+      {:httpoison, "~> 1.8"},
+      # {:hackney, "~> 4.7", override: true},
+      # {:decimal, "~> 3.1", override: true},
+      {:poison, "~> 3.1"},
+      {:observer_cli, "~> 1.6"},
+      {:cachex, "~> 3.3"},
+      {:phoenix_live_view, "~> 0.20"},
+      {:floki, ">= 0.27.0", only: :test},
+      # Ironwood support lives in this fork
+      {:zcashex, github: "dismad/zcashex", branch: "main"},
+      {:timex, "~> 3.0"},
+      {:sizeable, "~> 1.0"},
+      {:eqrcode, "~> 0.1.8"},
+      {:contex, "~> 0.3.0"},
+      {:muontrap, "~> 0.6.1"}
+    ]
+  end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
