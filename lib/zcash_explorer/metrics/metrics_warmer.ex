56debbf147f5b186125291e710babe1b2fdb0002
dismad <81990132+dismad@users.noreply.github.com>
2026-07-23 17:38:21 -0700

    Update metrics_warmer.ex
    update for slower nodes

--- lib/zcash_explorer/metrics/metrics_warmer.ex
+++ lib/zcash_explorer/metrics/metrics_warmer.ex
@@ -1,22 +1,39 @@
 defmodule ZcashExplorer.Metrics.MetricsWarmer do
   use Cachex.Warmer
+  require Logger
 
   @doc """
   Returns the interval for this warmer.
   """
-  def interval,
-    do: :timer.seconds(15)
+  def interval, do: :timer.seconds(15)
 
   @doc """
   Executes this cache warmer.
   """
   def execute(_state) do
-    Zcashex.getblockchaininfo() |> handle_result()
+    case safe_getblockchaininfo(60_000) do
+      {:ok, info} ->
+        handle_result({:ok, info})
+
+      {:error, reason} ->
+        Logger.warning("MetricsWarmer: #{inspect(reason)}")
+        :ignore
+    end
+  end
+
+  defp safe_getblockchaininfo(timeout) do
+    try do
+      case GenServer.call(Zcashex, {:call_endpoint, :getblockchaininfo}, timeout) do
+        {:ok, info} -> {:ok, info}
+        other -> {:error, other}
+      end
+    catch
+      :exit, reason -> {:error, reason}
+    end
   end
 
   # ignores the warmer result in case of error
-  defp handle_result({:error, _reason}),
-    do: :ignore
+  defp handle_result({:error, _reason}), do: :ignore
 
   defp handle_result({:ok, info}) do
     {:ok, [{"metrics", info}]}
