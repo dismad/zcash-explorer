eed2ff1530ab96a29849a820d11e61fb67dd976d
dismad <81990132+dismad@users.noreply.github.com>
2026-07-23 17:39:37 -0700

    Update networksolps_warmer.ex
    update

--- lib/zcash_explorer/metrics/networksolps_warmer.ex
+++ lib/zcash_explorer/metrics/networksolps_warmer.ex
@@ -1,22 +1,40 @@
 defmodule ZcashExplorer.Metrics.NetworkSolpsWarmer do
   use Cachex.Warmer
+  require Logger
 
   @doc """
   Returns the interval for this warmer.
   """
-  def interval,
-    do: :timer.seconds(15)
+  def interval, do: :timer.seconds(15)
 
   @doc """
   Executes this cache warmer.
   """
   def execute(_state) do
-    Zcashex.getnetworksolps() |> handle_result()
+    case safe_getnetworksolps(60_000) do
+      {:ok, info} ->
+        handle_result({:ok, info})
+
+      {:error, reason} ->
+        Logger.warning("NetworkSolpsWarmer: #{inspect(reason)}")
+        :ignore
+    end
+  end
+
+  defp safe_getnetworksolps(timeout) do
+    try do
+      # Matches earlier timeout stack: getnetworksolps with [120, -1]
+      case GenServer.call(Zcashex, {:call_endpoint, "getnetworksolps", [120, -1]}, timeout) do
+        {:ok, info} -> {:ok, info}
+        other -> {:error, other}
+      end
+    catch
+      :exit, reason -> {:error, reason}
+    end
   end
 
   # ignores the warmer result in case of error
-  defp handle_result({:error, _reason}),
-    do: :ignore
+  defp handle_result({:error, _reason}), do: :ignore
 
   defp handle_result({:ok, info}) do
     {:ok, [{"networksolps", info}]}
